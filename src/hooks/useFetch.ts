6e0088f1cfcdecf3732939ed4d58c9d22c00fb85
hmetkel-ontic <hmetkel@ontic.co>
2024-03-14 16:28:32 +0530

    [FEAT] use fetch hooks implementation

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const useFetch = (url: string, fetchOptions: any) => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const abortController = new AbortController();
+  const signal = abortController.signal;
+
+  async function fetchData(url: string, fetchOptions: any) {
+    try {
+      const response = await fetch(url, { ...fetchOptions, signal });
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    fetchData(url, fetchOptions)
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  return { isLoading, error, data };
+};
